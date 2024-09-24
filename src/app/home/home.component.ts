ec7135d6c843a757f0af1206815ebeef8af73fdb
ddcx9576 <glpat-BakzHz3DWQfND7_iuRau>
2024-09-24 14:44:17 +0100

    feat: add login component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { SignupComponent } from '../signup/signup.component';
 import { ModalService } from '../modal/modal.service';
 import { ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
+import { LoginComponent } from '../login/login.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -45,6 +46,30 @@ export class HomeComponent implements OnInit {
     //this.dialog.open(ForgotPasswordComponent,dialogConfig);
 
     this.modalService.open(ForgotPasswordComponent, {
+      animations: {
+        modal: {
+          enter: 'enter-scaling 0.3s ease-out',
+          leave: 'fade-out 0.1s forwards',
+        },
+        overlay: {
+          enter: 'fade-in 1s',
+          leave: 'fade-out 0.3s forwards',
+        },
+      },
+      size: {
+        width: '40rem',
+        padding: '24px',
+        //zIndex: '111',
+      },
+    });
+  }
+
+  handleLoginAction(){
+    //const dialogConfig = new MatDialogConfig();
+    //dialogConfig.width = "550px";
+    //this.dialog.open(ForgotPasswordComponent,dialogConfig);
+
+    this.modalService.open(LoginComponent, {
       animations: {
         modal: {
           enter: 'enter-scaling 0.3s ease-out',
